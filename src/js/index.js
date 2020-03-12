bf5ef3cffdb7bbe2fb982832b6be8c86d080d1ee
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2020-03-12 21:40:29 +0700

    using object method and add time animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,71 +1,92 @@
-var speedyBar = document.getElementsByClassName("speedyBar");
-speedyBar = speedyBar[0];
-var counter = null,
-    satuan = null,
-    jumlah = null;
+// Jam Dihabiskan : 6
+function speedyFunc(wrap, bar, times, time) {
+    let one = wrap.offsetWidth / times;
 
-
-function start() {
-    // inisialisasi
-    var speedy = document.getElementsByClassName("speedy");
-    speedy = speedy[0];
-    console.log(speedy.offsetWidth);
-
-    var angka = document.getElementsByClassName("steps");
-    jumlah = angka[0].value;
-
-    satuan = speedy.offsetWidth / jumlah;
-
-    // menambahkan style baru
-    style = document.getElementsByTagName('style');
+    // menghapus style speedyKeyframes jika ada
+    let style = document.getElementsByTagName('style');
     if (style.length > 0) {
-        for (var i = 0; i < style.length; i++) {
-            style[i].parentNode.removeChild(style[i]);
+        for (let i = 0; i < style.length; i++) {
+            if (style[i].className == 'speedyKeyframes') {
+                style[i].parentNode.removeChild(style[i]);
+            }
         }
     }
-    var keyframe = '';
 
-    for (var i = 0; i < jumlah; i++) {
-        data = '@keyframes bar' + i + '{0%{width : ' + satuan * i + 'px;} 100%{ width : ' + satuan * (i + 1) + 'px}}';
+    // menambahkan keyframes baru yang akan ditambahkan sebagai stylesheet
+    let keyframe = '';
+    for (var i = 0; i < times; i++) {
+        data = '@keyframes bar' + i + '{0%{width : ' + one * i + 'px;} 100%{ width : ' + one * (i + 1) + 'px}}';
         keyframe += data;
     }
 
-    para = document.createElement('style');
+    // menciptakan element style baru dan menambahkan di 
+    let para = document.createElement('style');
     para.innerHTML = keyframe;
-    body = document.getElementsByTagName('body');
+    para.className = 'speedyKeyframes';
+    let body = document.getElementsByTagName('body');
     body = body[0];
 
     body.insertBefore(para, body.childNodes[0]);
 
-    // menciptakan tombol counter
-    var settings2 = document.getElementsByClassName("settings2");
-    settings2 = settings2[0];
-    para = document.createElement('button');
-    para.innerHTML = 'Lanjutkan';
-    para.onclick = function() {
-        maju();
+    // membuat object speedy 
+    const speedyObject = {
+        counter: 0,
+        width: one,
+        rep: times,
+        AnimateLength: time,
+        forward: function() {
+            let that = this;
+            bar.style.width = that.width * this.counter + 'px';
+            bar.style.animation = 'bar' + this.counter + ' ' + this.AnimateLength + 'ms';
+            this.counter++;
+            a = setTimeout(function() {
+                bar.style.animation = 'none 0s';
+                bar.style.width = that.width * that.counter + 'px';
+            }, this.AnimateLength);
+            if (this.counter >= this.rep) {
+                this.counter = 0;
+            }
+        }
     };
-    para.className = 'set';
 
-    // check apakah ada tombol counter lain
-    if (settings2.firstChild) {
-        settings2.innerHTML = '';
-    }
-    settings2.appendChild(para);
+    speedyObj = Object.create(speedyObject);
 
-    // set width bar ke 0px
-    counter = 0;
-    speedyBar.style.width = '0px';
+    // inisialisasi bar awal
+    bar.style.animation = 'none 0s';
+    bar.style.width = '0px';
 }
 
-function maju() {
-    speedyBar.style.animation = 'bar' + counter + ' 2s';
-    setTimeout(function() {
-        speedyBar.style.width = counter * satuan + 'px';
-    }, 2000);
-    counter++;
-    if (counter == jumlah) {
-        counter = 0;
-        speedyBar.style.width = '0px';
-    }
-}
+
+var startButton = document.getElementsByClassName('startButton'),
+    counterButton = document.getElementsByClassName('counterButton');
+startButton = startButton[0];
+counterButton = counterButton[0];
+counterButton.setAttribute('disabled', true);
+
+
+startButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    // inisialisasi
+
+    let speedy = document.getElementsByClassName("speedy");
+    speedy = speedy[0];
+
+    let speedyBar = document.getElementsByClassName("speedyBar");
+    speedyBar = speedyBar[0];
+
+    let angka = document.getElementsByName("speedySet");
+    angka = angka[0].value;
+
+    let waktu = document.getElementsByName("speedySpeed");
+    waktu = waktu[0].value;
+
+
+    speedyFunc(speedy, speedyBar, angka, waktu);
+    counterButton.removeAttribute('disabled');
+});
+
+counterButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    speedyObj.forward();
+
+});
